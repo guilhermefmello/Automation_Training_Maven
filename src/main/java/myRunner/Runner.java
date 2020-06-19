e8da1ff2d935c85c4cfe26666ab2dda6b1142221
Guilherme Ferreira Mello <guilhermefmello@hotmail.com>
2020-06-19 16:29:33 +0100

    adding Feature and Runner packs and files

---
+++ src/main/java/myRunner/Runner.java
@@ -0,0 +1,23 @@
+package myRunner;
+
+import org.junit.runner.RunWith;
+import cucumber.api.junit.Cucumber;
+import cucumber.api.CucumberOptions;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "C:\\Users\\66578\\eclipse-workspace\\UKFT\\src\\main\\java\\Features\\startUKCWF.feature" //the path of feature files
+		,glue= {"stepDefinitions"}, //the path of step definitions files
+		plugin = {
+				"html:target/cucumber-reports", "pretty",
+				"json:target/cucumber-reports/CucumberTestReport.json" }, //to generate different types of reports
+		monochrome = true, //displays the console output in a proper readable format
+		//strict = true, //it will check if any step is not defined in step definition file
+		dryRun = false //will check if the steps in the feature file matches the steps in the step definition file
+		) 
+
+
+public class Runner {
+
+}
