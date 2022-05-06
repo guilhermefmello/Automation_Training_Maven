6de7c1033622aa8f6e916ca45695079586c09521
Guilherme Ferreira Mello <guilhermefmello@hotmail.com>
2022-05-06 15:00:16 +0100

    Adding dependency in POM file

--- src/main/java/stepDefinitions/Scenario_1_Step_Definition.java
+++ src/main/java/stepDefinitions/Scenario_1_Step_Definition.java
@@ -7,9 +7,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.CapabilityType;
-import org.openqa.selenium.remote.DesiredCapabilities;
-
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
